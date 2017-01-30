3e52fb30ac454e1de195fca2401a5acee8352d2b
InJung Chung <mu29@yeoubi.net>
2017-01-30 17:37:45 +0900

    메인에서 모든 파일을 로드하도록 수정한다

--- main.rb
+++ main.rb
@@ -1,32 +1,22 @@
 require 'gosu'
 
-class GameWindow < Gosu::Window
-  def initialize(width=800, height=600, full_screen=false)
-    super
-    self.caption = '로열로드 온라인'
-    @x = @y = 10
-    @draws = 0
-    @message = Gosu::Image.from_text(
-      self, info, Gosu.default_font_name, 30
-    )
-  end
+root_dir = File.dirname(__FILE__)
+require_pattern = File.join(root_dir, '**/*.rb')
+@failed = []
 
-  def update
-    @x -=1 if button_down?(Gosu::KbLeft)
-    @x +=1 if button_down?(Gosu::KbRight)
-    @y -=1 if button_down?(Gosu::KbUp)
-    @y +=1 if button_down?(Gosu::KbDown)
-  end
-
-  def draw
-    @draws += 1
-    @message.draw(@x, @y, 0)
-  end
-
-  def info
-    "x:#{@x} y:#{@y} draws:#{@draws}"
+Dir.glob(require_pattern).each do |f|
+  next if f.end_with?('/main.rb')
+  begin
+    require_relative f.gsub("#{root_dir}/", '')
+  rescue
+    @failed << f
   end
 end
 
-window = GameWindow.new
-window.show
+@failed.each do |f|
+  require_relative f.gsub("#{root_dir}/", '')
+end
+
+$window = Window.new
+Scene.switch(LoginScene.new)
+$window.show
