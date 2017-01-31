a953a18964b89bf7ab08750db54a93cf34f982cc
InJung Chung <mu29@yeoubi.net>
2017-01-31 18:47:24 +0900

    `FileManager` 에 `.path` 함수를 추가한다

--- app/core/game/file_manager.rb
+++ app/core/game/file_manager.rb
@@ -1,8 +1,17 @@
+require 'json'
+
 class FileManager
   class << self
-    def load(file)
-      File.join(File.dirname(File.dirname(File.dirname(
-        __FILE__))), 'data', file)
+    def path(file)
+      File.join(self.root_dir, 'data', file)
+    end
+
+    def root_dir
+      self.dirname(__FILE__, 4)
+    end
+
+    def dirname(dir, count)
+      count == 1 ? File.dirname(dir) : self.dirname(File.dirname(dir), count - 1)
     end
   end
 end
