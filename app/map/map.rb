332de83c909ed9ae601f9818bb0f395e62a3a258
InJung Chung <mu29@yeoubi.net>
2017-02-02 23:43:09 +0900

    맵을 그린다

--- app/map/map.rb
+++ app/map/map.rb
@@ -1,5 +1,6 @@
 class Map
   TILE_SIZE = 32
+
   attr_reader :id
   attr_reader :name
   attr_accessor :objects
@@ -44,13 +45,14 @@ class Map
         (x0..x1).each do |x|
           (y0..y1).each do |y|
             tile_id = @map[x][y][z]
-            # todo : tileset 정보 가지고 priority 맞는지 확인하기
-            if tile_id
-              tile = @map[x][y]
-              map_x = x * TILE_SIZE
-              map_y = y * TILE_SIZE
-              tile.draw(map_x, map_y, 0) if tile
-            end
+            next unless tile_id
+            next if @tileset.priorities[tile_id] != p
+            tile_id -= 384
+            next if tile_id < 0
+            tile = @tileset.image[tile_id]
+            map_x = x * TILE_SIZE
+            map_y = y * TILE_SIZE
+            tile.draw(map_x, map_y, 0) if tile
           end
         end
       end
@@ -76,15 +78,11 @@ class Map
       @height.times do |y|
         map[x][y] = {}
         3.times do |z|
-          map[x][y][z] = 0
+          map[x][y][z] = @data[x + y * @width + z * @width * @height]
         end
       end
     end
     map
-  end
-
-  def choose_tile
-    @tiles[0]
   end
 end
 
