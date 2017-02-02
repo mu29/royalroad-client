d46d57c953ff4706e06cd7c6d447c1a6e928054a
JUBIN PARK <jubin-park@naver.com>
2017-02-02 12:12:42 +0900

    `tools` 폴더는 require 예외 대상

--- main.rb
+++ main.rb
@@ -1,12 +1,11 @@
 require 'gosu'
 
-puts "!"
-
 root_dir = File.dirname(__FILE__)
 require_pattern = File.join(root_dir, '**/*.rb')
 @failed = []
 
 Dir.glob(require_pattern).each do |f|
+  next if f.include?(root_dir + "/tools") # except `tools` folder
   next if f.end_with?('/main.rb')
   begin
     require_relative f.gsub("#{root_dir}/", '')
