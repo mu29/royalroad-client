b0a8a30e93d2778b6088a7afc543fb5e0297daf5
InJung Chung <mu29@yeoubi.net>
2017-02-05 14:08:41 +0900

    `CharacterGraphics#current_image` 로 이동 모션을 보여준다

--- app/character/character_graphics.rb
+++ app/character/character_graphics.rb
@@ -9,7 +9,19 @@ class CharacterGraphics < Component
   end
 
   def draw(viewport)
-    return unless viewport.include?(x, y, @width * 2, @height * 2)
-    @image[0].draw(x, y, 0)
+    return unless viewport.include?(x, y, @width, @height)
+    current_image.draw(x, y, 0)
+  end
+
+  private
+
+  def current_image
+    physics = object.physics
+    direction = physics.direction
+    current_frame = (direction / 2 - 1) * 4
+    if physics.moving?
+      current_frame += (physics.vertical? ? object.y : object.x) % 64 / 16 
+    end
+    @image[current_frame]
   end
 end
